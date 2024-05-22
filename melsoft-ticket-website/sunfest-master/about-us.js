ec970a65c26881b44724c3af544bdc8f86d39a00
miyaggiemiami <clementmorare971@gmail.com>
2024-05-22 23:56:47 +0200

    added videos, see more button

--- melsoft-ticket-website/sunfest-master/about-us.js
+++ melsoft-ticket-website/sunfest-master/about-us.js
@@ -1,14 +1,48 @@
-let count = document.getElementsByClassName("count");
-let inc = [];
+class Counter {
+  constructor(element) {
+    this.element = element;
+    this.increment = 1;
+    this.maxData = parseInt(this.element.getAttribute("max-data"));
+  }
 
-function intervalFunc(){
-  for(let i=0; i<count.length; i++){
-    inc.push(1);
-    if(inc[i] != count[i].getAttribute("max-data")){
-      inc[i]++;
+  update() {
+    if (this.increment < this.maxData) {
+      this.increment++;
+      this.element.innerHTML = this.increment;
     }
-    count[i].innerHTML = inc[i];
   }
 }
 
-intervalFunc();
+let counters = [];
+
+function intervalFunc() {
+  for (let i = 0; i < counters.length; i++) {
+    counters[i].update();
+  }
+}
+
+setInterval(intervalFunc, 85);
+
+let countElements = document.getElementsByClassName("count");
+
+for (let i = 0; i < countElements.length; i++) {
+  counters.push(new Counter(countElements[i]));
+}
+
+const videos = document.querySelectorAll('.video');
+
+for (let i = 0; i < videos.length; i++) {
+  videos[i].addEventListener('loadedmetadata', () => {
+    const width = videos[i].videoWidth;
+    const height = videos[i].videoHeight;
+    const aspectRatio = width / height;
+
+    if (width > height) {
+      videos[i].style.width = '33.33%';
+      videos[i].style.height = 'auto';
+    } else {
+      videos[i].style.width = 'auto';
+      videos[i].style.height = '33.33vh';
+    }
+  });
+}
