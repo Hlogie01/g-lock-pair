0cc4f34a1a1d98b48df34a4becd6ada31b7f0644
OmolemoModipane <modipane101@gmail.com>
2024-05-26 18:30:16 +0200

    search button

---
+++ melsoft-ticket-website/sunfest-master/search.js
@@ -0,0 +1,26 @@
+document.getElementById("searchButton").addEventListener("click", function () {
+  // Get the value entered in the search input
+  var searchTerm = document.getElementById("searchInput").value.toLowerCase();
+
+  // Redirect based on search term
+  switch (searchTerm) {
+    case "ticket":
+      window.location.href = "ticket.html";
+      break;
+    case "cottonfest":
+      window.location.href = "cottonfest.html";
+      break;
+    case "burna boy":
+      window.location.href = "burna.html";
+      break;
+    case "wine and dine":
+      window.location.href = "wine-events-page.html";
+      break;
+    case "artists":
+      window.location.href = "#artists-homepage-section";
+    default:
+      // If search term doesn't match any predefined case, you can handle it accordingly
+      alert("No match found for the search term.");
+      break;
+  }
+});
